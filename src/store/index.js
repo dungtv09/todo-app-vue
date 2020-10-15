689ee04f2e14818d25569ee237e419672217054f
Viet Dung <dungtv09@gmail.com>
2020-10-15 09:10:20 +0700

    add strict mode

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  strict: true,
+  strict: process.env.NODE_ENV !== "production",
   state: {
     todos: ["Todo 1", "Todo 2", "Todo 4"],
     completedTodos: ["Todo 3"],
